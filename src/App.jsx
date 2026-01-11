284cceda7f013927525e9f2b1bd26403a0990473
Dmitri Power <160439126+xSantanxx@users.noreply.github.com>
2026-01-10 23:56:30 -0500

    removed the timer

--- src/App.jsx
+++ src/App.jsx
@@ -78,18 +78,18 @@ function App() {
             })
         }
 
-        useEffect(() => {
-            const controls = animate(count, 100, {duration: 5});
-
-            controls.then(() => {
-                setTimeout(() => {
-                    setVisible(!visible);
-                    setCountDown(true);
-                }, 150)
-            })
-
-            return () => controls.stop()
-        }, [])
+        // useEffect(() => {
+        //     const controls = animate(count, 100, {duration: 5});
+        //
+        //     controls.then(() => {
+        //         setTimeout(() => {
+        //             setVisible(!visible);
+        //             setCountDown(true);
+        //         }, 150)
+        //     })
+        //
+        //     return () => controls.stop()
+        // }, [])
 
 
     useEffect(() => {
@@ -197,15 +197,18 @@ function App() {
             setLoading(false);
         }
 
+        useEffect(() => {
+            setVisible(!visible);
+        }, [])
 
         useEffect( () => {
 
-            if(!countDown){
-                return;
-            }
+            // if(!countDown){
+            //     return;
+            // }
 
             getInfo();
-        }, [countDown]);
+        }, []);
 
     const signInFailed = () => {
         toast.error("Please enter in the details", {
@@ -357,7 +360,7 @@ function App() {
         return (
             <>
             <div className={'flex justify-center w-full'}>
-                <motion.pre animate={{opacity: countDown ? 0 : 1}} transition={{duration: 0.5}} className={`text-4xl flex justify-center items-center text-purple-200 h-dvh fixed inset-0 z-100 pointer-events-none`}>{rounded}</motion.pre>
+                {/*<motion.pre animate={{opacity: countDown ? 0 : 1}} transition={{duration: 0.5}} className={`text-4xl flex justify-center items-center text-purple-200 h-dvh fixed inset-0 z-100 pointer-events-none`}>{rounded}</motion.pre>*/}
                 <div className={`${!visible ? 'hidden' : ''} min-w-1/5 max-w-full rounded-2xl z-10 mt-30 ml-auto mr-auto absolute flex flex-col justify-center items-center shadow-2xl backdrop-blur-md h-1/2 bg-white/10`}>
                     {/*Sign Up Sheet*/}
                     <h1 className={'text-lg text-shadow-2xs'}>Sign Up</h1>
